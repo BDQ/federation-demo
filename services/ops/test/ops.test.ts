79b1345298d2ca98fda45eda8402b71fa6d7b6ff
Simon Walsh <simon.walsh@glossier.com>
2020-01-08 10:14:49 -0500

    Adding lerna, fixing a mis-import

--- services/ops/test/ops.test.ts
+++ services/ops/test/ops.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import cdk = require('@aws-cdk/core');
-import Ops = require('../lib/ops-stack');
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle
+} from "@aws-cdk/assert";
+import cdk = require("@aws-cdk/core");
+import Ops = require("../lib/gql-stack");
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Ops.OpsStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Ops.GQLStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {}
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
